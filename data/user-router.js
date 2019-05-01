5fc7c0ecc8dbfa6bf784c783b2e8b8f75900a82a
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-01 17:51:11 -0400

    basic CRUD operation for user route is working

--- data/user-router.js
+++ data/user-router.js
@@ -13,7 +13,35 @@ userRouter.get('/', async (req, res) => {
     } 
 })
 
+userRouter.get('/:id', async (req, res) => {
+    try{
+        const user = await Users.getById(req.params.id);
+        user ? res.status(200).json({user}) : res.status(404).json({ message: "no one here by that ID"})
+    }catch(err){res.status(500).json({errorMessage: err })}
+})
 
+userRouter.post('/', async (req, res) => {
+    try{
+        const newUser = await Users.insert(req.body)
+        const { id, name } =req.body;
+        id && name ? res.status(200).json(newUser) : res.status(304).json({message: " nope, just no"})
+    }catch(err){res.status(500).json({errorMessage: err })}
+})
+
+userRouter.delete('/:id', async (req, res) => {
+    try{
+        const userId = await Users.remove(req.params.id)
+        userId ? res.status(200).json({ message: "user has been terminated "}) : res.status(400).json({ message: " everyone deserrve a second chance, even when they do not exist"})
+    }catch(err){res.status(500).json({errorMessage: err })}
+})
+
+userRouter.put('/:id', async (req, res) => {
+    try{
+        const updatedUser = await Users.update(req.params.id, req.body);
+        const { id, name } = req.body;
+        name && id ? res.status(200).json(updatedUser) : res.status(400).json({ message: "not really, keep working"})
+    }catch(err){res.status(500).json({errorMessage: err })}
+})
 
 userRouter.use((req, res, next) => {
     res.status(404).json({ message: "Nice try, but no"})
